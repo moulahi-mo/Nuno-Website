8dae02b9aa68cef72e8643ce46f0da97399a3b48
moulahi-mo <moulahi.mo@gmail.com>
2020-12-01 02:09:10 +0100

    finishing the 40% project

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// ! scroll background show
+
+$(window).scroll(function () {
+  if (scrollY > 600) {
+    $("#mainNav").addClass("darken");
+  } else {
+    $("#mainNav").removeClass("darken");
+    $("#homebtn").addClass("active");
+  }
+});
+
+//  !Init Scrollspy
+$("body").scrollspy({ target: "#mainNav" });
+
+// ! Smooth Scrolling
+//  $("#mainNav a").on('click', function (event) {
+//    if (this.hash !== "") {
+//      event.preventDefault();
+
+//      const hash = this.hash;
+
+//      $('html, body').animate({
+//        scrollTop: $(hash).offset().top
+//      }, 800, function () {
+
+//        window.location.hash = hash;
+//      });
+//    }
+//  });
