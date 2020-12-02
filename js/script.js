8459b48865864b592ec2374382314fc392e6fa6f
moulahi-mo <moulahi.mo@gmail.com>
2020-12-02 01:18:32 +0100

    finishing all html template

--- js/script.js
+++ js/script.js
@@ -27,3 +27,22 @@ $("body").scrollspy({ target: "#mainNav" });
 //      });
 //    }
 //  });
+
+// ! caroussel owl
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
